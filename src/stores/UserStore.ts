4fb9f7110de99f515336fa41382693f14f101570
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-12-27 16:54:28 -0300

    creating sign up form

---
+++ src/stores/UserStore.ts
@@ -0,0 +1,16 @@
+import BaseStore from "./BaseStore";
+import UserDomain from '../domains/user'
+import UserService from '../services/user'
+
+class UserStore extends BaseStore {
+  constructor() {
+    super(UserService)
+
+  }
+
+  initializeData() {
+    return new UserDomain();
+  }
+}
+
+export default UserStore
