4a607f2428949c20773b0b14b258f406c7aecf9b
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2022-01-17 10:02:19 -0300

    #20 - fix

--- src/pages/profile/form.tsx
+++ src/pages/profile/form.tsx
@@ -3,16 +3,6 @@ import { observer } from 'mobx-react'
 
 @observer
 class ProfileForm extends React.Component {
-  // constructor() {
-  //   super()
-  // }
-
-  componentDidMount() {
-    // const { setTitle } = this.props
-    // setTitle(HOME.text)
-    // this.store.init()
-  }
-
   render() {
     return (
       <div>
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,16 +3,6 @@ import { observer } from 'mobx-react'
 
 @observer
 class profileIndex extends React.Component {
-  // constructor() {
-  //   super()
-  // }
-
-  componentDidMount() {
-    // const { setTitle } = this.props
-    // setTitle(HOME.text)
-    // this.store.init()
-  }
-
   render() {
     return (
       <div>
